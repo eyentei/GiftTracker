009891871b0719a29fec9443b57c6725b16aef1b
kokosha98 <kokosha.98@yandex.ru>
2017-12-06 22:54:11 +0300

    Add files via upload
    + library of classes

---
+++ GiftTrackerClasses/Gift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftTrackerClasses
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public Person Owner { get; set; }
+        public Occasion Occasion { get; set; }
+    }
+}
